2ab154134be26ee260ce953fe4896c2a9e788aed
Stexlthy <80535996+Stexlthy@users.noreply.github.com>
2021-10-18 16:04:12 +0300

    Add files via upload

---
+++ simple id checker.py
@@ -0,0 +1,34 @@
+# Basic Tutorial
+# Create two files in the same folder and name them "ids" and "working ids" (You can change their names by editing line 7 and 8)
+# Deprecated until further notice
+
+import requests
+
+w = open("working ids.txt", "a")
+f = open("ids.txt", "r")
+
+def is_steam_customurl_taken(id):
+    s = requests.Session()
+    r = s.get("https://steamcommunity.com/id/%s" % id)
+    if ("The specified profile could not be found.".lower() in r.text.lower()):
+        return False
+    return True
+
+print("Checking ids." + "\n")
+
+lines = f.readlines()
+for line in lines:
+    username = line.strip()
+    is_taken = is_steam_customurl_taken(username)
+    if is_taken:
+        print("%s is under usage" % username)
+    else:
+        print("%s it's possibly not under use" % username)
+        w.write(username + "\n")
+
+w.close()
+f.close()
+
+print("Finalized search")
+print("Open your working ids(or whatever you named it to) folder to check them out")
+input("Close me")
